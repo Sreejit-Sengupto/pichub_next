24c568b02c8fa328d869cb006e54c1ae43b08a9c
Sreejit-Sengupto <sreesen2003@gmail.com>
2024-06-22 22:13:15 +0530

    Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,10 +28,10 @@ export default function Home() {
                 }}
                 className='relative flex flex-col gap-4 items-center justify-center px-4'
             >
-                <div className='text-3xl md:text-7xl font-bold text-primary text-center'>
+                <div className='text-5xl md:text-7xl font-bold text-primary text-center'>
                     Pichub
                 </div>
-                <div className='font-extralight text-base md:text-4xl dark:text-neutral-200 py-4'>
+                <div className='font-extralight text-xl md:text-4xl dark:text-neutral-200 py-4'>
                     Your Memories, Your Gallery, Your Friends
                 </div>
 
