47a9c2fd836e5a35b7241fc81c737cb15100e02c
Sreejit-Sengupto <sreesen2003@gmail.com>
2024-06-22 22:15:38 +0530

    link fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
                         Login
                     </Link>
                     <Link
-                        href={'/sign-up'}
+                        href={'/signup'}
                         className='bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2'
                     >
                         Get started
