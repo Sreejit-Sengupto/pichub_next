d86cb942bfebce89e958319d6a2adbc00df8a7dd
Sreejit-Sengupto <sreesen2003@gmail.com>
2024-06-22 22:08:42 +0530

    Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 'use client';
+import React from 'react';
 import { useAuth } from '@/components/auth-provider';
-import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import { motion } from 'framer-motion';
+import { AuroraBackground } from '@/components/ui/aurora-background';
 
 export default function Home() {
     const router = useRouter();
@@ -16,59 +17,39 @@ export default function Home() {
     }, [user, router]);
 
     return (
-        <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-            <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-                <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-                    Get started by signing up&nbsp;
-                    <code className='font-mono font-bold'>
-                        <Link href={'/signup'}>Signup</Link>
-                    </code>
-                </p>
-                {/* <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none'>
-                    <a
-                        className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
-                        href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        By{' '}
-                        <Image
-                            src='/vercel.svg'
-                            alt='Vercel Logo'
-                            className='dark:invert'
-                            width={100}
-                            height={24}
-                            priority
-                        />
-                    </a>
-                </div> */}
-            </div>
-
-            <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-                <div className='flex flex-col justify-center items-center'>
-                    <p className='font-bold text-4xl lg:text-6xl'>Pichub</p>
-                    <p className='text-center'>
-                        View and share photos with your friends
-                    </p>
+        <AuroraBackground>
+            <motion.div
+                initial={{ opacity: 0.0, y: 40 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                    delay: 0.3,
+                    duration: 0.8,
+                    ease: 'easeInOut',
+                }}
+                className='relative flex flex-col gap-4 items-center justify-center px-4'
+            >
+                <div className='text-3xl md:text-7xl font-bold text-primary text-center'>
+                    Pichub
                 </div>
-            </div>
+                <div className='font-extralight text-base md:text-4xl dark:text-neutral-200 py-4'>
+                    Your Memories, Your Gallery, Your Friends
+                </div>
 
-            <div className='mb-32 text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:text-left mx-auto flex justify-center items-center'>
-                <Link
-                    href='/login'
-                    className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-                >
-                    <h2 className='mb-3 text-2xl font-semibold'>
-                        Login{' '}
-                        <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className='m-0 max-w-[30ch] text-sm opacity-50'>
-                        Already Registered? Login Now!
-                    </p>
-                </Link>
-            </div>
-        </main>
+                <div>
+                    <Link
+                        href={'/login'}
+                        className='bg-black dark:bg-transparent rounded-full w-fit text-white px-7 py-2 border border-gray-500 mr-2'
+                    >
+                        Login
+                    </Link>
+                    <Link
+                        href={'/sign-up'}
+                        className='bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2'
+                    >
+                        Get started
+                    </Link>
+                </div>
+            </motion.div>
+        </AuroraBackground>
     );
 }
